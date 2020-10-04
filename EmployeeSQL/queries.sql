3dc7a2f06b234cf898d3887c366c9c3666e4eed3
Gabriel Pivaro <gabrconc@gmail.com>
2020-10-04 15:50:49 -0500

    Database Script

---
+++ EmployeeSQL/queries.sql
@@ -0,0 +1,23 @@
+SELECT *
+FROM employees
+LIMIT (5);
+
+SELECT *
+FROM departments
+LIMIT (5);
+
+SELECT *
+FROM departments_employees
+LIMIT (5);
+
+SELECT *
+FROM managers
+LIMIT (5);
+
+SELECT *
+FROM salaries
+LIMIT (5);
+
+SELECT *
+FROM titles
+LIMIT (5);
